fb4395b61fe63f73cb44ec433adf8442035de889
Isadora Aguiar <isaadoraguiar@hotmail.com>
2024-03-16 00:05:59 -0300

    update img src

--- src/components/List/Empty.tsx
+++ src/components/List/Empty.tsx
@@ -3,7 +3,10 @@ import styles from './Empty.module.css'
 export function Empty() {
   return (
     <div className={styles.emptyList}>
-      <img src="/public/clipboard.png" alt="Ícone de prancheta" />
+      <img
+        src="https://i.ibb.co/D8M2H86/clipboard.png"
+        alt="Ícone de prancheta"
+      />
       <p>
         <strong>Você ainda não tem tarefas cadastradas</strong>
         Crie tarefas e organize seus itens a fazer
